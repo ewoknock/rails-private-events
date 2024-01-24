d854363052bd0cf997063f46f4ad638740950700
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-01-24 15:38:58 -0800

    Adds in reference from Events model to the User who created the Event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_24_193647) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_24_233625) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_193647) do
     t.datetime "date_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_193647) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end

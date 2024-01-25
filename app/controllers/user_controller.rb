f33807155989a9f7f2f219bb2e32246c339c43c4
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-01-24 16:33:38 -0800

    Creates temporary placeholder show action for user model and creates routes

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,9 @@
+class UserController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @created_events = current_user.created_events
+    @attended_events = current_user.attended_events
+  end
+
+end

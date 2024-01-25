08687f072ffe7eabaa74e1f6874b040069bca4fa
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-01-24 16:37:19 -0800

    Modifies login form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,8 +2,8 @@ class UserController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @created_events = current_user.created_events
-    @attended_events = current_user.attended_events
+    #@created_events = current_user.created_events
+    #@attended_events = current_user.attended_events
   end
 
 end

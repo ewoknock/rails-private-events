8f816bc372f6023d7fceac02b77601c821cf94b4
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-01-26 17:38:02 -0800

    Fixed model associations between events and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
-  has_many :attended_events, foreign_key: :attended_event_id
-  has_many :events_attended, through: :attended_events
+  has_many :event_attendees, foreign_key: :attended_event_id
+  has_many :events_attended, through: :event_attendees, source: :event_attendee
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

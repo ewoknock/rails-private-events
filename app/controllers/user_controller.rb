0ed0ffaf9acb366d3f87cdde0e7a7f281656ebca
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-01-26 17:27:02 -0800

    Adds in user show page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,7 @@ class UserController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    #@created_events = current_user.created_events
+    @created_events = current_user.created_events
     #@attended_events = current_user.attended_events
   end
 

a7e89af7df214fb869a8826a249897586ad8e454
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-01-29 11:49:39 -0800

    Fixes user#show variable for attendees and removes Invitees and Attendees from the event cards

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,7 @@ class UserController < ApplicationController
 
   def show
     @created_events = current_user.created_events
-    @attended_events = current_user.events_attended
+    @attended_events = current_user.attended_events
   end
 
 end

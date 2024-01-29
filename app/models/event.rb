9de40493d21e4ad2a7a72866791ad016417db717
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-01-29 11:47:11 -0800

    Fixed associations between User, Event, and EventAttendees to function correwclty

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, foreign_key: :creator_id, class_name: 'User'
-  has_many :event_attendees, foreign_key: :event_attendee_id
-  has_many :attendees, through: :event_attendees, source: :attended_event
+  has_many :event_attendees, foreign_key: :attended_event_id
+  has_many :attendees, through: :event_attendees, source: :event_attendee
 
   scope :past, -> { where('date_time < ?', Date.today) }
   scope :upcoming, -> { where('date_time > ?', Date.today) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
   has_many :event_attendees, foreign_key: :event_attendee_id
-  has_many :attended_events, through: :event_attendees, source: :event_attendee
+  has_many :attended_events, through: :event_attendees
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
